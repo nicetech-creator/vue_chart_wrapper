5cbf5343ce60ae21aaac066daca74d46ba18b052
Top Tech <56525446+nicetech-creator@users.noreply.github.com>
2020-08-02 22:57:29 +0800

    deploy

---
+++ README.md
@@ -0,0 +1,13 @@
+## Requirements
+
+* [Node.js](http://nodejs.org/)
+* [Vue.js](https://vuejs.org/)
+* [Vue CLI](https://cli.vuejs.org/)
+
+## Installation Steps
+
+1. Clone repo
+2. Run `npm install`
+3. Run `npm run serve`
+4. Visit http://localhost:8080/
+
---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import 'chart.js'
+import 'hchs-vue-charts'
+import Chartkick from 'chartkick'
+import VueChartkick from 'vue-chartkick'
+
+Vue.use(window.VueCharts)
+Vue.use(VueChartkick, { Chartkick })
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
